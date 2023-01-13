3c591cf9ea7b453cd7456693e29ec8cbde9fc1e2
leego <leegodev@hotmail.com>
2023-01-13 11:02:18 +0800

    Add `maven-assembly-plugin`

--- minimap/src/main/java/com/spiralstudio/minimap/Main.java
+++ minimap/src/main/java/com/spiralstudio/minimap/Main.java
@@ -33,7 +33,7 @@ public class Main {
                             })))
                     .installOnByteBuddyAgent();
         } catch (Throwable cause) {
-            throw new Error("Failed to load SpiralPing", cause);
+            throw new Error("Failed to load Minimap", cause);
         }
     }
 
