dd236e09200944d078dbdb9400c9971b3bcc0516
leego <leegodev@hotmail.com>
2023-01-16 17:23:24 +0800

    Add `pandorabox`

---
+++ pandorabox/src/main/java/com/spiralstudio/mod/pandorabox/Main.java
@@ -0,0 +1,21 @@
+package com.spiralstudio.mod.pandorabox;
+
+import com.threerings.projectx.client.ProjectXApp;
+
+/**
+ * @author Leego Yih
+ * @see com.threerings.projectx.item.client.ArsenalPanel
+ */
+public class Main {
+    static {
+        try {
+            // TODO
+        } catch (Throwable cause) {
+            throw new Error("Failed to load mod 'PandoraBox'", cause);
+        }
+    }
+
+    public static void main(String[] args) {
+        ProjectXApp.main(args);
+    }
+}
