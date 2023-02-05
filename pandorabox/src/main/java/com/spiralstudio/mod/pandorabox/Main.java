852ca0716e505a5938ee45c369ed368747deb233
leego <leegodev@hotmail.com>
2023-02-05 15:28:08 +0800

    Fix pandorabox

--- pandorabox/src/main/java/com/spiralstudio/mod/pandorabox/Main.java
+++ pandorabox/src/main/java/com/spiralstudio/mod/pandorabox/Main.java
@@ -21,14 +21,28 @@ public class Main {
             CtClass ctClass = classPool.get("com.threerings.projectx.shop.client.l");
             CtMethod ctMethod = ctClass.getDeclaredMethod("KD");
             ctMethod.setBody("{" +
-                    "com.threerings.projectx.shop.data.ShopDialogInfo sdi = this.aQw;\n" +
-                    "com.threerings.projectx.shop.data.UniqueShopInfo usi = this.aQx;\n" +
-                    "System.out.println(\"[ShopDialog] shop name: \" + sdi.name + \", shop title: \" + sdi.title);\n" +
+                    "System.out.println(\"ShopDialogInfo: \"+$0.aQw.toString());\n" +
+                    "System.out.println(\"UniqueShopInfo: \"+$0.aQx.toString());\n" +
+                    "com.threerings.projectx.shop.data.ShopDialogInfo sdi = $0.aQw;\n" +
+                    "com.threerings.projectx.shop.data.UniqueShopInfo usi = $0.aQx;\n" +
+                    "try {\n" +
+                    "    Object name = com.threerings.projectx.shop.data.ShopDialogInfo.class.getField(\"name\").get(sdi);\n" +
+                    "    Object title = com.threerings.projectx.shop.data.ShopDialogInfo.class.getField(\"title\").get(sdi);\n" +
+                    "    System.out.println(\"[ShopDialog] shop name: \" + name + \", shop title: \" + title);\n" +
+                    "    } catch (Exception e) {\n" +
+                    "        throw new RuntimeException(e);\n" +
+                    "    }\n" +
                     "for (int i = 0; i < usi.goodCounts.size(); i++) {\n" +
                     "    com.threerings.projectx.shop.data.UniqueShopInfo.GoodCount gc = usi.goodCounts.get(i);\n" +
-                    "    System.out.println(\"[ShopDialog] index: \" + Integer.toString(i) + \", good: \" + gc.good.toString() +\", count: \" + Integer.toString(gc.count));\n" +
+                    "    try {\n" +
+                    "        Object good = com.threerings.projectx.shop.data.UniqueShopInfo.GoodCount.class.getField(\"good\").get(gc);\n" +
+                    "        Object count = com.threerings.projectx.shop.data.UniqueShopInfo.GoodCount.class.getField(\"count\").get(gc);\n" +
+                    "        System.out.println(\"[ShopDialog] index: \" + Integer.toString(i) + \", good: \" + good.toString() +\", count: \" + count.toString());\n" +
+                    "    } catch (Exception e) {\n" +
+                    "        throw new RuntimeException(e);\n" +
+                    "    }\n" +
                     "}\n" +
-                    "return this.aQx;\n" +
+                    "return $0.aQx;\n" +
                     "}");
             ctClass.toClass();
             ctClass.detach();
