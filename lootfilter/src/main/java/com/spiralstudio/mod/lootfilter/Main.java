a3510bf2525282f03eff5474a42844a9ecc51617
leego <leegodev@hotmail.com>
2023-03-01 12:06:38 +0800

    Update `lootfilter`

--- lootfilter/src/main/java/com/spiralstudio/mod/lootfilter/Main.java
+++ lootfilter/src/main/java/com/spiralstudio/mod/lootfilter/Main.java
@@ -9,13 +9,14 @@ import com.spiralstudio.mod.core.util.MethodModifier;
 
 import java.lang.reflect.Modifier;
 import java.util.Arrays;
-import java.util.Collection;
+import java.util.Collections;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 /**
  * @author Leego Yih
  * @see com.threerings.presents.dobj.MessageEvent
+ * @see com.threerings.projectx.dungeon.client.a.A PickupSprite
  */
 public class Main {
     private static boolean mounted = false;
@@ -30,34 +31,162 @@ public class Main {
         }
         mounted = true;
         Config config = Configs.read("lootfilter", Config.class);
-        redefineMessageEvent(config);
+        boolean added = addOffsetImplClass(config);
+        if (!added) {
+            return;
+        }
+        redefinePickupSprite();
+        redefineMessageEvent();
     }
 
-    static void redefineMessageEvent(Config config) throws Exception {
-        if (config == null || config.getMessage() == null) {
-            return;
+    static boolean addOffsetImplClass(Config config) throws Exception {
+        if (config == null) {
+            return false;
         }
-        if (config.getMessage().getType() == null && config.getMessage().getName() == null) {
-            return;
+        Config.Pickup pickup = config.getPickup();
+        Config.Message message = config.getMessage();
+        if (pickup == null && message == null) {
+            return false;
         }
-        Config.Message.Filter type = config.getMessage().getType();
-        Config.Message.Filter name = config.getMessage().getName();
-        Set<String> excludedTypes = type != null ? type.getExcluded() : null;
-        Set<String> includedTypes = type != null ? type.getIncluded() : null;
-        Set<String> excludedNames = name != null ? name.getExcluded() : null;
-        Set<String> includedNames = name != null ? name.getIncluded() : null;
-        // Override class `com.threerings.presents.dobj.MessageEvent`
-        ClassBuilder.fromClass("com.threerings.presents.dobj.MessageEvent")
+        ClassBuilder.makeClass("com.spiralstudio.mod.lootfilter.LootFilter")
+                .addField(new FieldBuilder()
+                        .fieldName("_inited")
+                        .typeName("boolean")
+                        .modifiers(Modifier.PRIVATE | Modifier.STATIC | Modifier.VOLATILE))
                 .addFields(Arrays.stream(new String[]{
-                                "__lootExcludedTypes",
-                                "__lootIncludedTypes",
-                                "__lootExcludedNames",
-                                "__lootIncludedNames"})
+                                "_pickupExcludedTypes", "_pickupIncludedTypes", "_pickupExcludedNames", "_pickupIncludedNames",
+                                "_messageExcludedTypes", "_messageIncludedTypes", "_messageExcludedNames", "_messageIncludedNames"})
                         .map(e -> new FieldBuilder()
                                 .fieldName(e)
-                                .typeName("java.util.HashSet")
-                                .modifiers(Modifier.PUBLIC | Modifier.STATIC | Modifier.TRANSIENT | Modifier.VOLATILE))
+                                .typeName("java.util.Set")
+                                .modifiers(Modifier.PRIVATE | Modifier.STATIC))
                         .collect(Collectors.toList()))
+                .addMethod(new MethodBuilder()
+                        .body("" +
+                                "public static void init(java.lang.Object[] args) {\n" +
+                                "    if (args.length != 8) {\n" +
+                                "        return;\n" +
+                                "    }\n" +
+                                "    if (_inited) {\n" +
+                                "        return;\n" +
+                                "    }\n" +
+                                "    synchronized (com.spiralstudio.mod.lootfilter.LootFilter.class) {\n" +
+                                "        if (_inited) {\n" +
+                                "            return;\n" +
+                                "        }\n" +
+                                "        _inited = true;\n" +
+                                "        _pickupExcludedTypes = (java.util.Set) args[0];\n" +
+                                "        _pickupIncludedTypes = (java.util.Set) args[1];\n" +
+                                "        _pickupExcludedNames = (java.util.Set) args[2];\n" +
+                                "        _pickupIncludedNames = (java.util.Set) args[3];\n" +
+                                "        _messageExcludedTypes = (java.util.Set) args[4];\n" +
+                                "        _messageIncludedTypes = (java.util.Set) args[5];\n" +
+                                "        _messageExcludedNames = (java.util.Set) args[6];\n" +
+                                "        _messageIncludedNames = (java.util.Set) args[7];\n" +
+                                "    }\n" +
+                                "}"))
+                .addMethod(new MethodBuilder()
+                        .body("" +
+                                "public static boolean isPickupExcluded(String type, String name) {\n" +
+                                "    if (name != null) {\n" +
+                                "        String nameLowerCase = name.toLowerCase();\n" +
+                                "        if (_pickupIncludedNames.contains(nameLowerCase)) {\n" +
+                                "            return false;\n" +
+                                "        } else if (_pickupExcludedNames.contains(nameLowerCase)) {\n" +
+                                "            return true;\n" +
+                                "        }\n" +
+                                "    }\n" +
+                                "    if (type != null) {\n" +
+                                "        String typeLowerCase = type.toLowerCase();\n" +
+                                "        if (_pickupIncludedTypes.contains(typeLowerCase)) {\n" +
+                                "            return false;\n" +
+                                "        } else if (_pickupExcludedTypes.contains(typeLowerCase)) {\n" +
+                                "            return true;\n" +
+                                "        }\n" +
+                                "    }\n" +
+                                "    return false;\n" +
+                                "}"))
+                .addMethod(new MethodBuilder()
+                        .body("" +
+                                "public static boolean isMessageExcluded(String type, String name) {\n" +
+                                "    if (name != null) {\n" +
+                                "        String nameLowerCase = name.toLowerCase();\n" +
+                                "        if (_messageIncludedNames.contains(nameLowerCase)) {\n" +
+                                "            return false;\n" +
+                                "        } else if (_messageExcludedNames.contains(nameLowerCase)) {\n" +
+                                "            return true;\n" +
+                                "        }\n" +
+                                "    }\n" +
+                                "    if (type != null) {\n" +
+                                "        String typeLowerCase = type.toLowerCase();\n" +
+                                "        if (_messageIncludedTypes.contains(typeLowerCase)) {\n" +
+                                "            return false;\n" +
+                                "        } else if (_messageExcludedTypes.contains(typeLowerCase)) {\n" +
+                                "            return true;\n" +
+                                "        }\n" +
+                                "    }\n" +
+                                "    return false;\n" +
+                                "}"))
+                .build();
+
+        Set[] args = new Set[]{
+                pickup != null && pickup.getType() != null ? pickup.getType().getExcluded() : null,
+                pickup != null && pickup.getType() != null ? pickup.getType().getIncluded() : null,
+                pickup != null && pickup.getName() != null ? pickup.getName().getExcluded() : null,
+                pickup != null && pickup.getName() != null ? pickup.getName().getIncluded() : null,
+                message != null && message.getType() != null ? message.getType().getExcluded() : null,
+                message != null && message.getType() != null ? message.getType().getIncluded() : null,
+                message != null && message.getName() != null ? message.getName().getExcluded() : null,
+                message != null && message.getName() != null ? message.getName().getIncluded() : null,
+        };
+        for (int i = 0; i < args.length; i++) {
+            if (args[i] == null) {
+                args[i] = Collections.emptySet();
+            }
+        }
+        Class.forName("com.spiralstudio.mod.lootfilter.LootFilter")
+                .getDeclaredMethod("init", new Class[]{java.lang.Object[].class})
+                .invoke(null, new Object[]{args});
+        return true;
+    }
+
+    static void redefinePickupSprite() throws Exception {
+        // Override class `com.threerings.projectx.dungeon.client.a.PickupSprite`
+        ClassBuilder.fromClass("com.threerings.projectx.dungeon.client.a.A")
+                .modifyMethod(new MethodModifier()
+                        .methodName("a")
+                        .paramTypeNames("com.threerings.tudey.config.ActorSpriteConfig")
+                        .insertAfter("" +
+                                "if (this.Zj != null) {\n" +
+                                "    com.threerings.projectx.util.A _ctx = (com.threerings.projectx.util.A) this.aqE;\n" +
+                                "    com.threerings.projectx.item.data.Item _item = this.Zj;\n" +
+                                "    String _name = _item.b(_ctx);\n" +
+                                "    String _type = null;\n" +
+                                "    com.threerings.projectx.item.config.ItemConfig.Original _original = _item.y(_ctx.getConfigManager());\n" +
+                                "    if (_original != null) {\n" +
+                                "        com.threerings.projectx.item.data.ItemCodes.Group _group = _original.BA();\n" +
+                                "        if (_group != null) {\n" +
+                                "            com.threerings.util.N _bundle = _ctx.getMessageManager().dI(\"dungeon\");\n" +
+                                "            if (_bundle != null) {\n" +
+                                "                _type = _bundle.bu(_group.toString());\n" +
+                                "            }\n" +
+                                "        }\n" +
+                                "    }\n" +
+                                "    boolean _excluded = (boolean) Class.forName(\"com.spiralstudio.mod.lootfilter.LootFilter\")\n" +
+                                "        .getDeclaredMethod(\"isPickupExcluded\", new java.lang.Class[]{java.lang.String.class, java.lang.String.class})\n" +
+                                "        .invoke(null, new java.lang.Object[]{_type, _name});" +
+                                "    if (_excluded) {\n" +
+                                "        this._model.setVisible(false);\n" +
+                                "        this._model.clearConfig();\n" +
+                                "        super.Eo();\n" +
+                                "    }\n" +
+                                "}"))
+                .build();
+    }
+
+    static void redefineMessageEvent() throws Exception {
+        // Override class `com.threerings.presents.dobj.MessageEvent`
+        ClassBuilder.fromClass("com.threerings.presents.dobj.MessageEvent")
                 // Override method `com.threerings.presents.dobj.MessageEvent.notifyListener`
                 .modifyMethod(new MethodModifier()
                         .methodName("aO")
@@ -79,20 +208,6 @@ public class Main {
                                 "    if (!\"crowd.chat\".equals(event.getName())) {\n" +
                                 "        return false;\n" +
                                 "    }\n" +
-                                "    if (__lootExcludedTypes == null) {\n" +
-                                "        synchronized (com.threerings.presents.dobj.MessageEvent.class) {\n" +
-                                "            if (__lootExcludedTypes == null) {\n" +
-                                "                __lootExcludedTypes = new java.util.HashSet();\n" +
-                                "                __lootIncludedTypes = new java.util.HashSet();\n" +
-                                "                __lootExcludedNames = new java.util.HashSet();\n" +
-                                "                __lootIncludedNames = new java.util.HashSet();\n" +
-                                "                java.util.Collections.addAll(__lootExcludedTypes, ((Object[]) " + buildLootStringArray(excludedTypes) + "));\n" +
-                                "                java.util.Collections.addAll(__lootIncludedTypes, ((Object[]) " + buildLootStringArray(includedTypes) + "));\n" +
-                                "                java.util.Collections.addAll(__lootExcludedNames, ((Object[]) " + buildLootStringArray(excludedNames) + "));\n" +
-                                "                java.util.Collections.addAll(__lootIncludedNames, ((Object[]) " + buildLootStringArray(includedNames) + "));\n" +
-                                "            }\n" +
-                                "        }\n" +
-                                "    }\n" +
                                 "    java.lang.Object[] _args = event.getArgs();\n" +
                                 "    if (_args == null || _args.length == 0 || _args[0] == null || !(_args[0] instanceof com.threerings.crowd.chat.data.ChatMessage)) {\n" +
                                 "        return false;\n" +
@@ -109,40 +224,13 @@ public class Main {
                                 "    if (_bundle == null) {\n" +
                                 "        return false;\n" +
                                 "    }\n" +
-                                "    String itemType = _bundle.bu(ss[1]);\n" +
-                                "    String itemName = _bundle.bu(ss[2]);\n" +
-                                "    if (itemName != null) {\n" +
-                                "        itemName = itemName.toLowerCase();\n" +
-                                "        if (__lootIncludedNames.contains(itemName)) {\n" +
-                                "            return false;\n" +
-                                "        } else if (__lootExcludedNames.contains(itemName)) {\n" +
-                                "            return true;\n" +
-                                "        }\n" +
-                                "    }\n" +
-                                "    if (itemType != null) {\n" +
-                                "        itemType = itemType.toLowerCase();\n" +
-                                "        if (__lootIncludedTypes.contains(itemType)) {\n" +
-                                "            return false;\n" +
-                                "        } else if (__lootExcludedTypes.contains(itemType)) {\n" +
-                                "            return true;\n" +
-                                "        }\n" +
-                                "    }\n" +
-                                "    return true;\n" +
+                                "    String _itemType = _bundle.bu(ss[1]);\n" +
+                                "    String _itemName = _bundle.bu(ss[2]);\n" +
+                                "    return (boolean) Class.forName(\"com.spiralstudio.mod.lootfilter.LootFilter\")\n" +
+                                "        .getDeclaredMethod(\"isMessageExcluded\", new java.lang.Class[]{java.lang.String.class, java.lang.String.class})\n" +
+                                "        .invoke(null, new java.lang.Object[]{_itemType, _itemName});" +
                                 "}"))
                 .build();
-    }
-
-    static String buildLootStringArray(Collection<String> c) {
-        if (c == null) {
-            return "new String[0]";
-        }
-        StringBuilder sb = new StringBuilder("new String[]{");
-        for (String s : c) {
-            sb.append("\"").append(s.toLowerCase()).append("\",");
-        }
-        sb.deleteCharAt(sb.length() - 1);
-        sb.append("}");
-        return sb.toString();
     }
 
     public static class Config {
@@ -166,7 +254,24 @@ public class Main {
         }
 
         public static class Pickup {
+            private Filter type;
+            private Filter name;
 
+            public Filter getType() {
+                return type;
+            }
+
+            public void setType(Filter type) {
+                this.type = type;
+            }
+
+            public Filter getName() {
+                return name;
+            }
+
+            public void setName(Filter name) {
+                this.name = name;
+            }
         }
 
         public static class Message {
@@ -188,26 +293,26 @@ public class Main {
             public void setName(Filter name) {
                 this.name = name;
             }
+        }
 
-            public static class Filter {
-                private Set<String> excluded;
-                private Set<String> included;
+        public static class Filter {
+            private Set<String> excluded;
+            private Set<String> included;
 
-                public Set<String> getExcluded() {
-                    return excluded;
-                }
+            public Set<String> getExcluded() {
+                return excluded;
+            }
 
-                public void setExcluded(Set<String> excluded) {
-                    this.excluded = excluded;
-                }
+            public void setExcluded(Set<String> excluded) {
+                this.excluded = excluded;
+            }
 
-                public Set<String> getIncluded() {
-                    return included;
-                }
+            public Set<String> getIncluded() {
+                return included;
+            }
 
-                public void setIncluded(Set<String> included) {
-                    this.included = included;
-                }
+            public void setIncluded(Set<String> included) {
+                this.included = included;
             }
         }
     }
