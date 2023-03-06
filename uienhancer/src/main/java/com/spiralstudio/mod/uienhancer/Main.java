dbf156178c394b27c3f44e45c204654cba0abb19
leego <leegodev@hotmail.com>
2023-03-06 11:36:42 +0800

    Add test code

--- uienhancer/src/main/java/com/spiralstudio/mod/uienhancer/Main.java
+++ uienhancer/src/main/java/com/spiralstudio/mod/uienhancer/Main.java
@@ -3,7 +3,9 @@ package com.spiralstudio.mod.uienhancer;
 import com.spiralstudio.mod.core.ClassPool;
 import com.spiralstudio.mod.core.Commands;
 import com.spiralstudio.mod.core.Registers;
+import com.spiralstudio.mod.core.util.ConstructorModifier;
 import com.spiralstudio.mod.core.util.MethodBuilder;
+import com.spiralstudio.mod.core.util.MethodModifier;
 
 /**
  * TODO
@@ -23,8 +25,53 @@ public class Main {
             return;
         }
         mounted = true;
-        redefineHudWindow();
-        addCommands();
+        redefineMap();
+        //redefineHudWindow();
+        //addCommands();
+    }
+
+    static void redefineMap() {
+        // Show monsters on map
+        ClassPool.from("com.threerings.projectx.map.client.a")
+                .modifyConstructor(new ConstructorModifier()
+                        .paramTypeNames("com.threerings.tudey.util.m", "com.threerings.tudey.a.k")
+                        .insertAfter("" +
+                                "this.aMi.dY(com.threerings.projectx.data.ProjectXCodes.avh | com.threerings.editor.swing.editors.MaskEditor.n(\"collision\", \"player\") | com.threerings.editor.swing.editors.MaskEditor.n(\"collision\", \"player_barrier\") | com.threerings.editor.swing.editors.MaskEditor.n(\"collision\", \"monster\"));\n"));
+        // Zoom map
+        ClassPool.from("com.threerings.projectx.map.client.c")
+                .modifyConstructor(new ConstructorModifier()
+                        .paramTypeNames("com.threerings.tudey.util.m", "com.threerings.tudey.a.k")
+                        .body("" +
+                                "        {\n" +
+                                "            super($1, true);\n" +
+                                "            com.threerings.tudey.util.m var1 = $1;\n" +
+                                "            com.threerings.tudey.a.k var2 = $2;\n" +
+                                "            this.aqE = var1;\n" +
+                                "            this._view = var2;\n" +
+                                "            this.aMq = this.b(this.aqE, this._view);\n" +
+                                "            if (this.aMq != null) {\n" +
+                                "                this.add(this.aMq, new java.lang.Integer(4));\n" +
+                                "            }\n" +
+                                "            if (this.aMq != null) {\n" +
+                                "                com.threerings.opengl.gui.e.c var3 = this.aMq.getPreferredSize(-1, -1);\n" +
+                                "                var3.width = this.aqE.getRoot().getDisplayWidth() * 10;\n" +
+                                "                var3.height = this.aqE.getRoot().getDisplayHeight() * 10;\n" +
+                                "                this.aMq.setPreferredSize(var3);\n" +
+                                "                this.aMq.setSize(var3.width, var3.height);\n" +
+                                "                this.pack();\n" +
+                                "            }\n" +
+                                "            this.setVisible(true);\n" +
+                                "            this.setHoverable(false);\n" +
+                                "        }"))
+                .modifyMethod(new MethodModifier()
+                        .methodName("bQ")
+                        .paramTypeNames("boolean")
+                        .insertBefore("" +
+                                "if (this.aMq != null) {\n" +
+                                "    com.threerings.opengl.gui.e.c var3 = this.aMq.getPreferredSize(-1, -1);\n" +
+                                "    System.out.println(\"Width:\" + Integer.toString(var3.width) + \", Height:\" + Integer.toString(var3.height));" +
+                                "}\n" +
+                                "System.out.println(\"Show map: \" + Boolean.toString($1));\n"));
     }
 
     static void redefineHudWindow() {
